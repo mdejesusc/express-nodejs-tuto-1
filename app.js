cc1092546ba62c2316e59308f1667269a37ca8b7
Martín de Jesus Camacho <mdejesusc@gmail.com>
2019-02-14 19:26:58 -0600

    express-nodejs-tuto-1 initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+/*
+app.get('/', (req, res) => {
+    res.end('Works!');
+});
+*/
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log("Server Started!");
+});
